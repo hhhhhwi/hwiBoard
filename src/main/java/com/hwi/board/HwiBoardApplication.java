a4ab71e883838a0ee6645ed895de61c700c217df
hwiyeon0831 <aab555586@gmail.com>
2023-06-22 00:27:56 +0900

    #6 [enhancement] 스프링 부트 프로젝트 세팅 - 스프링 부트 프로젝트 생성 - gitignore 파일 생성

---
+++ src/main/java/com/hwi/board/HwiBoardApplication.java
@@ -0,0 +1,13 @@
+package com.hwi.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwiBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwiBoardApplication.class, args);
+    }
+
+}
