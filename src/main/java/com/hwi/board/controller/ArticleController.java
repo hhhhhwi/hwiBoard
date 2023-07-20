44025fc1ae11ca723315bde3a37e1f41ed91767c
hwiyeon0831 <aab555586@gmail.com>
2023-07-21 01:27:13 +0900

    #17 [enhancement] 뷰 기본 골격 작성 - index view html 골격 작성 - header, footer 분리 - index 응답에 대한 Get 메소드 작성

--- src/main/java/com/hwi/board/controller/ArticleController.java
+++ src/main/java/com/hwi/board/controller/ArticleController.java
@@ -1,10 +1,20 @@
 package com.hwi.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap model) {
+        model.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
