f18b5c1a6034464f02ab9a7315e7a2fa5971bc55
hwiyeon0831 <aab555586@gmail.com>
2023-07-22 12:59:00 +0900

    #17 [enhancement] thymeleaf decoupled logic 설정 추가 - java config로 thymeleaf3의 decoupled logic 설정 - https://www.thymeleaf.org/doc/tutorials/3.1/usingthymeleaf.html#configuring-decoupled-templates

--- src/main/java/com/hwi/board/HwiBoardApplication.java
+++ src/main/java/com/hwi/board/HwiBoardApplication.java
@@ -2,7 +2,10 @@ package com.hwi.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class HwiBoardApplication {
 
