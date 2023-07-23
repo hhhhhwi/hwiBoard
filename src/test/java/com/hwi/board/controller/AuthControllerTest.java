11cba8e8c8fd4a1468a428d5992790e908d715f9
hwiyeon0831 <aab555586@gmail.com>
2023-07-23 22:14:33 +0900

    #17 [enhancement] 인증 관련 Test 추가 - 기존 Test에 SecurityConfig import 추가 - 인증 Controller Test 클래스 및 메소드 추가

---
+++ src/test/java/com/hwi/board/controller/AuthControllerTest.java
@@ -0,0 +1,37 @@
+package com.hwi.board.controller;
+
+import com.hwi.board.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("인증 관련 Controller 테스트")
+@Import(SecurityConfig.class)
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+    private final MockMvc mockMvc;
+
+    public AuthControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("로그인 뷰 테스트")
+    @Test
+    public void givenNothing_whenTryingToLogin_thenReturnsLoginView() throws Exception {
+        // Given
+
+        // When & Then
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
